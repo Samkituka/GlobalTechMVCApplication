a482de4a8c73ae0ebbfb98240ea327a2c3955601
Samuel Kituka <s.kituka1@gmail.com>
2023-01-22 23:51:27 -0500

    Updated GlobalTech.DataAccess
    Updated C# Class Library GlobalTech.DataAccess which is the Project Reference to GlobalTechMVCApplication

---
+++ GlobalTech.DataAccess/Context/GlobalTechDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GlobalTech.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTech.DataAccess.Context
+{
+    public class GlobalTechDbContext : DbContext
+    {
+        public GlobalTechDbContext (DbContextOptions <GlobalTechDbContext> options) :base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; } = null!;
+       
+    }
+}
---
+++ GlobalTech.DataAccess/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTech.DataAccess.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        [Required(ErrorMessage = "This field is required.")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; } = null!;
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName ("Emp.Code")]
+        public string EmpCode { get; set; } = null!;
+        [Column(TypeName ="nvarchar(100)")]
+        public string Position { get; set; } = null!;
+        [Column(TypeName = "nvarchar (100)")]
+        [DisplayName ("Office Location")]
+        public string OfficeLocation { get; set; } = null!; 
+
+    }
+}
